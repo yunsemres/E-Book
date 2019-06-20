72018b7ac709846dcfc5b2e2c773b33741a53e49
yunsemres <yunus_sutcu@hotmail.com>
2019-06-20 16:24:39 +0300

    Add project files.

---
+++ EBook.UI/Areas/Member/Controllers/ArticleController.cs
@@ -0,0 +1,47 @@
+﻿using EBook.Service.Option;
+using EBook.UI.Areas.Member.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBook.UI.Areas.Member.Controllers
+{
+    public class ArticleController : Controller
+    {
+        ArticleService _articleService;
+        CategoryService _categoryService;
+        AppUserService _appUserService;
+        CommentService _commentService;
+        LikeService _likeService;
+
+        public ArticleController()
+        {
+            _articleService = new ArticleService();
+            _categoryService = new CategoryService();
+            _appUserService = new AppUserService();
+            _commentService = new CommentService();
+            _likeService = new LikeService();
+        }
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Show(Guid id)
+        {
+            // vm içine verileri gönderelim..
+            ArticleDetailVM model =new ArticleDetailVM();
+            model.AppUser = _appUserService.GetById(model.Article.AppUser.ID);// tekil
+            model.Article = _articleService.GetById(id);// tekil
+            model.Comments = _commentService.GetDefault(x => x.ArticleID == id && (x.Status == Core.Enum.Status.Active || x.Status == Core.Enum.Status.Updated));
+            model.likes = _likeService.GetDefault(x => x.ArticleID == id && (x.Status == Core.Enum.Status.Active || x.Status == Core.Enum.Status.Updated));
+            model.LikeCount = _likeService.GetDefault(x => x.ArticleID == id && (x.Status == Core.Enum.Status.Active || x.Status == Core.Enum.Status.Updated)).Count;
+            model.CommentCount = _commentService.GetDefault(x => x.ArticleID == id && (x.Status == Core.Enum.Status.Active || x.Status == Core.Enum.Status.Updated)).Count;
+
+            return View(model);
+
+        }
+    }
+}
---
+++ EBook.UI/Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using EBook.Service.Option;
+using EBook.UI.Areas.Member.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBook.UI.Areas.Member.Controllers
+{
+    public class HomeController : Controller
+    {
+        ArticleService _articleService;
+        AppUserService _appUserService;
+        CommentService _commentService;
+        LikeService _likeService;
+        public HomeController()
+        {
+            _articleService = new ArticleService();
+            _appUserService = new AppUserService();
+            _commentService = new CommentService();
+            _likeService = new LikeService();
+        }
+        public ActionResult Index()
+        {
+            ArticleDetailVM model = new ArticleDetailVM();
+            model.Articles = _articleService.GetActive();
+            foreach (var item in model.Articles)
+            {
+                model.Comments = _commentService.GetDefault(x => x.ArticleID == item.ID).OrderByDescending(x => x.CreatedDate).Take(10).ToList();
+                model.CommentCount = _commentService.GetDefault(x => x.ArticleID == item.ID).Count();
+                model.LikeCount = _likeService.GetDefault(x => x.ArticleID == item.ID).Count();
+
+            }
+            return View(model);
+        }
+
+       
+    }
+}
